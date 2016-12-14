3be8af5bdb018468ebd273cfeedef826328f1b6b
judsonc <judson.scosta@hotmail.com>
2016-12-13 23:46:24 -0300

    login web ok

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,28 +2,29 @@ import {Component} from 'react';
 import {hashHistory} from 'react-router'
 
 export default class Login extends Component {
+  componentDidMount() {
+    localStorage.setItem('proximaVisita','/login')
+  }
+
   render() {
-    const url = "https://garcomajato-d9f5d.firebaseapp.com/"
+    const url = 'https://garcomajato-d9f5d.firebaseapp.com/'
     if (navigator.onLine && window.cordova) {
-      var ref = window.open(url, '_blank', 'location=no,zoom=no,clearcache=yes');
+      let ref = window.open(url, '_blank', 'location=no,zoom=no');
       ref.addEventListener('loadstart', function(event) {
-        if (event.url !== url && event.url.indexOf("gaj_user_token=") > 0) {
-          let id = event.url.split("gaj_user_token=")
-          alert("id " + id[1])
+        if (event.url !== url && event.url.indexOf('gaj_user_token=') > 0) {
+          let id = event.url.split('gaj_user_token=')
+          localStorage.setItem('gaj_user_token',id[1])
           ref.close()
-          alert("deu certo")
-          hashHistory.push("ambiente")
+          hashHistory.push('ambiente')
         }
       });
-      // ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-      ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-      // ref.addEventListener('exit', function(event) {
-      //   if (event.type == "exit"){
-      //
-      //   }
-      // });
+      ref.addEventListener('exit', function(event) {
+        if (event.type === "exit" && !localStorage.getItem('gaj_user_token')) {
+          window.reload()
+        }
+      });
     } else {
-      alert("Voce está sem net")
+      alert("Você está desconectado!")
     }
     return null
   }
