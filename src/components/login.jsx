8188ea12ca57bb81f99120ecf5f41e47129deb6d
judsonc <judson.scosta@hotmail.com>
2016-12-14 03:28:12 -0300

    login com loading

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,15 +1,20 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import {hashHistory} from 'react-router'
+import RefreshIndicator from 'material-ui/RefreshIndicator'
 
 export default class Login extends Component {
-  componentDidMount() {
-    localStorage.setItem('proximaVisita','/login')
+  constructor() {
+    super()
+    this.state = {
+      showLogin: false,
+    }
   }
 
-  render() {
+  popupLogin() {
+    let self = this
     const url = 'https://garcomajato-d9f5d.firebaseapp.com/'
-    if (navigator.onLine && window.cordova) {
-      let ref = window.open(url, '_blank', 'location=no,zoom=no');
+    if (window.cordova) {
+      let ref = window.open(url, '_blank', 'location=no,zoom=no,toolbar=no,hidden=yes');
       ref.addEventListener('loadstart', function(event) {
         if (event.url !== url && event.url.indexOf('gaj_user_token=') > 0) {
           let id = event.url.split('gaj_user_token=')
@@ -17,15 +22,49 @@ export default class Login extends Component {
           ref.close()
           hashHistory.push('ambiente')
         }
-      });
+      })
+      ref.addEventListener('loadstop', function(event) {
+        self.setState({ showLogin: true })
+        ref.show()
+      })
       ref.addEventListener('exit', function(event) {
-        if (event.type === "exit" && !localStorage.getItem('gaj_user_token')) {
-          window.reload()
+        if (!localStorage.getItem('gaj_user_token')) {
+          self.setState({ showLogin: false })
+          self.popupLogin()
         }
-      });
+      })
     } else {
-      alert("Você está desconectado!")
+      alert("Você não está usando um celular")
     }
-    return null
+  }
+
+  componentDidMount() {
+    localStorage.setItem('proximaVisita','/login')
+    this.popupLogin()
+  }
+
+  render() {
+    const style = {
+      container: {
+        position: 'relative',
+      },
+      refresh: {
+        position: 'relative',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    }
+
+    return (this.state.showLogin) ? null : (
+      <div style={style.container}>
+        <RefreshIndicator
+          size={100}
+          left={0}
+          top={130}
+          status='loading'
+          style={style.refresh}
+        />
+      </div>
+    )
   }
 }
